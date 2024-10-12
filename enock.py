8659a219b675ca0c93b77e289329e12f64630db9
Enock Mugisha <e.mugisha4@alustudent.com>
2024-10-12 23:07:41 +0200

    Add files via upload

---
+++ enock.py
@@ -0,0 +1,19 @@
+#!/bin/python
+# Creating a hash map (dictionary) in Python
+
+enock={}
+# Define the put function
+def put(dictionary, key, value):
+    dictionary[key] = value
+
+# Using the put function to add key-value pairs
+put(enock, "enock", "football")
+put(enock, "mugisha", "volleyball")
+put(enock, "benon", "basketball")
+
+# Print the dictionary
+print(enock)
+
+# Accessing values using keys
+print("What they like:", enock["mugisha"])
+print("What they like:", enock["benon"])
