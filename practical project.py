97565031a01bb4ba79b2063462da85d614351f88
Enock Mugisha <e.mugisha4@alustudent.com>
2024-10-12 22:40:13 +0200

    Add files via upload

---
+++ practical project.py
@@ -0,0 +1,15 @@
+def greeting(name,department):
+    print("you are welcome to this organisation, " +name)
+    print("you are part of this department:" +department)
+greeting("mugisha","softwateengineer" )
+greeting("ejide","IT MANAGER")
+list_1=[1,2,3 ,8,5]
+sorted(list_1)
+print (list_1)
+print(sorted((list_1)))
+print((min(list_1)))
+def area_triangle(base,height):
+   return base*height
+area1=area_triangle(7,8)
+print(area1)
+print(10!=1)
